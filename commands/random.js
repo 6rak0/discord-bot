66db1331d69ae71dc1eaa432c55dce749bf6e001
6rak0 <ego.6rak0@gmail.com>
2021-01-08 18:59:18 -0600

    records de speedrun

--- commands/random.js
+++ commands/random.js
@@ -8,12 +8,12 @@ module.exports = {
   usage: '<límite superior>',
   execute(message, args) {
     const limit = parseInt(args[0])
-    if (isNaN(limit) || limit === 0)
-      message.reply(`ese no es un número válido ${message.author}`)
+    if (isNaN(limit) || limit < 1)
+      message.channel.send(`ese no es un número válido ${message.author}`)
     else if (limit > 999999) message.reply(`el límite debe ser menor a 999999`)
     else {
       const random = Math.floor(Math.random() * Math.floor(limit))
-      message.reply(`te ha salido un ${random + 1} ${message.author}`)
+      message.reply(`te ha salido un ${random + 1}`)
     }
   },
 }
